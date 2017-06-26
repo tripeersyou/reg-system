f7a2f176f7ab9535446dab4545b8b867cc3140af
Francis Avancena <foavancena@gmail.com>
2017-06-26 19:58:04 +0800

    Added Bootstrap CSS

--- ajax_sample.php
+++ ajax_sample.php
@@ -23,13 +23,13 @@
     $members = json_encode($members);
 ?>
     <div class="sample">
-        ID Number: <input type="text" name="id_number" id="id_number" onchange='search(<?php echo $members ?>)'>
+        ID Number: <input class="form-control" type="text" name="id_number" id="id_number" onchange='search(<?php echo $members ?>)'>
         <br>
         <br>
-        First Name: <input type="text" name="first_name" id="first_name">
+        First Name: <input class="form-control" type="text" name="first_name" id="first_name">
         <br>
         <br>
-        Last Name: <input type="text" name="last_name" id="last_name">                
+        Last Name: <input class="form-control" type="text" name="last_name" id="last_name">                
     </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
